7470f30973e5d4497e77f27ca13d76773ad8ab64
white-devil5911 <muhammadqasim5911@gmail.com>
2026-05-31 12:34:51 +0500

    added updated cv inside portfolio

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -197,7 +197,7 @@ const Portfolio: FC = () => {
 
   const handleResumeDownload = (): void => {
     const link = document.createElement("a");
-    link.href = "https://docs.google.com/uc?export=download&id=1JFJLI2Mbq-Cb69FK3R55LQgE2NwMcGmu";
+    link.href = "https://docs.google.com/uc?export=download&id=1ocDI7LoT8NcUQIxjumHKLBcFrTGbS5t-";
     link.setAttribute("download", "Muhammad Qasim-CV.pdf");
     link.click();
   };
